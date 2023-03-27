11e6b27b6e25747add14b8753c5341e9cb9d93ab
Bruno Monteiro <brunomourabastos@gmail.com>
2023-03-27 20:36:15 -0300

    feat: Criação da lógica para retornar todos os dados cadastrados, informando CPF e respectivo Valor, por ordem de cadastro

--- service.js
+++ service.js
@@ -1,30 +1,42 @@
 
 
 const validarEntradaDeDados = (lancamento) => {
-   if (typeof lancamento.cpf !== 'string' && lancamento.cpf.length !== 11 && isNaN(lancamento.cpf)) {
+   const { cpf, valor } = lancamento;
+   if (typeof cpf !== 'string' && cpf.length !== 11 && isNaN(cpf)) {
       return 'CPF deve ser uma string com 11 caracteres numéricos'
    }
-   if (typeof lancamento.valor !== 'number' || lancamento.valor > 15000 || lancamento.valor < -2000) {
+   if (typeof valor !== 'number' || valor > 15000 || valor < -2000) {
       return 'Valor deve ser um número entre -2000 e 15000'
    }
    let Soma = 0;
-   if (lancamento.cpf === '00000000000') return 'CPF inválido';
-   for (i = 1; i <= 9; i++) Soma = Soma + parseInt(lancamento.cpf.substring(i - 1, i)) * (11 - i);
+   if (cpf === '00000000000') return 'CPF inválido';
+   for (i = 1; i <= 9; i++) Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
    let resto = (Soma * 10) % 11;
    if ((resto === 10) || (resto === 11)) resto = 0;
-   if (resto !== parseInt(lancamento.cpf.substring(9, 10))) return 'CPF inválido';
+   if (resto !== parseInt(cpf.substring(9, 10))) return 'CPF inválido';
    Soma = 0;
-   for (i = 1; i <= 10; i++) Soma = Soma + parseInt(lancamento.cpf.substring(i - 1, i)) * (12 - i);
+   for (i = 1; i <= 10; i++) Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
    resto = (Soma * 10) % 11;
    if ((resto === 10) || (resto === 11)) resto = 0;
-   if (resto !== parseInt(lancamento.cpf.substring(10, 11))) return 'CPF inválido';
+   if (resto !== parseInt(cpf.substring(10, 11))) return 'CPF inválido';
 
    return null;
 }
 
 
 const recuperarSaldosPorConta = (lancamentos) => {
-   return []
+   if (lancamentos.length === 0) {
+      return []
+   }
+   let allData = []
+   for (const lancamento of lancamentos) {
+      const { cpf, valor } = lancamento
+      allData.push({
+         cpf,
+         valor
+      })
+   }
+   return allData
 }
 
 const recuperarMaiorMenorLancamentos = (cpf, lancamentos) => {
